89e2fada542cfea215aaaa729f7852370ec05d91
SalathielGenese <salathielgenese@gmail.com>
2019-02-10 12:04:19 +0100

    removes comments

--- functions/src/contact-form.ts
+++ functions/src/contact-form.ts
@@ -4,9 +4,6 @@ import * as CORS from 'cors';
 
 
 const cors = CORS({ origin: true });
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
 export const contactForm = https.onRequest((request, response) => {
     cors( request, response, () => {
         response.json( request.body );
