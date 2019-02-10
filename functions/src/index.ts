286062e39a64b689e2ebba7604b5b65d0b45d825
SalathielGenese <salathielgenese@gmail.com>
2019-02-10 11:48:23 +0100

    implements the helloWorld function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,7 @@
-import * as functions from 'firebase-functions';
-
+import { https } from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
