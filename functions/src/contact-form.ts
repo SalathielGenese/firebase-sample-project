d54a335cbf8de7377e558db3e494f2c5b27ef35b
SalathielGenese <salathielgenese@gmail.com>
2019-02-10 12:02:55 +0100

    adds PING/PING contactForm function

---
+++ functions/src/contact-form.ts
@@ -0,0 +1,14 @@
+import { https } from 'firebase-functions';
+import * as CORS from 'cors';
+
+
+
+const cors = CORS({ origin: true });
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const contactForm = https.onRequest((request, response) => {
+    cors( request, response, () => {
+        response.json( request.body );
+    });
+});
