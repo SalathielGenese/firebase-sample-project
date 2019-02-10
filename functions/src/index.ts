b6941aa17bf4d8ed857e4b4169047d22418556f8
SalathielGenese <salathielgenese@gmail.com>
2019-02-10 11:53:06 +0100

    enable CORS origin

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,14 @@
 import { https } from 'firebase-functions';
+import * as CORS from 'cors';
+
+
+
+const cors = CORS({ origin: true });
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    cors( request, response, () => {
+        response.send("Hello from Firebase!");
+    });
 });
